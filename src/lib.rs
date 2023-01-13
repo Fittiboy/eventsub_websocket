def539a3b190f2ef8aed2f0703f85c6f27b46f58
Fitti <dev@fitti.io>
2023-01-13 08:14:32 +0100

    Enable basic message handling

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,29 @@ use std::net::TcpStream;
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
 use url::Url;
 
+pub struct Session {
+    socket: Socket,
+    id: String,
+    handled: Vec<String>,
+}
+
+impl Session {
+    pub fn new(socket: Socket) -> Session {
+        Session {
+            socket,
+            id: String::new(),
+            handled: vec![],
+        }
+    }
+}
+
 pub type Socket = WebSocket<MaybeTlsStream<TcpStream>>;
 
-pub fn get_session() -> Socket {
+pub fn get_session() -> Session {
     let (socket, _) = connect(Url::parse("wss://eventsub-beta.wss.twitch.tv/ws").unwrap())
         .expect("Cannot connect");
 
-    socket
+    Session::new(socket)
 
     // let _ = socket.write_message(Message::Text(
     //     r#"{
@@ -26,24 +42,59 @@ pub fn get_session() -> Socket {
 fn format_message(msg: Message) -> JsonValue {
     let data = msg.to_text().unwrap();
     let data = format!("[{}]", data);
-    json::parse(&data).unwrap()
+    json::parse(&data).unwrap()[0].to_owned()
 }
 
-pub fn handle_welcome(socket: &mut Socket) -> JsonValue {
+fn handle_welcome(msg: JsonValue, session: &mut Session) {
+    println!("Welcome!");
+    let id = &msg["payload"]["session"]["id"];
+    if !id.is_null() {
+        session.id = String::from(id.to_string());
+    };
+}
+
+fn handle_keepalive(msg: JsonValue) {
+    println!("Keepalive received: {}", msg);
+}
+
+fn handle_notification(msg: JsonValue) {
+    println!("Notifiaction received: {}", msg);
+}
+
+pub fn event_handler(session: &mut Session) {
     loop {
-        let msg = socket.read_message().expect("Error reading message");
+        let msg = session
+            .socket
+            .read_message()
+            .expect("Error reading message");
         let parsed = format_message(msg);
-        let id = &parsed[0]["payload"]["session"]["id"];
-        if !id.is_null() {
-            return parsed;
+
+        if parsed.is_null() {
+            println!("Empty message: {}", parsed);
+            continue;
         }
-    }
-}
 
-pub fn event_handler(socket: &mut Socket) {
-    loop {
-        let msg = socket.read_message().expect("Error reading message");
-        let parsed = format_message(msg);
+        let metadata = &parsed["metadata"];
+        let msg_id = String::from(&metadata["message_id"].to_string());
+
+        if session.handled.contains(&msg_id) {
+            println!("Duplicate message: {}", parsed);
+            continue;
+        }
+
+        let msg_type = &metadata["message_type"];
+
+        if msg_type == "session_welcome" {
+            handle_welcome(parsed, session);
+        } else if msg_type == "session_keepalive" {
+            handle_keepalive(parsed);
+        } else if msg_type == "notification" {
+            handle_notification(parsed);
+        } else {
+            println!("Received message: {}", parsed);
+        };
+
+        session.handled.push(msg_id);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use eventsub_websocket::{event_handler, get_session, handle_welcome};
+use eventsub_websocket::{event_handler, get_session};
 
 fn main() {
-    let mut socket = get_session();
-    let id = handle_welcome(&mut socket);
-    event_handler(&mut socket);
+    let mut session = get_session();
+    event_handler(&mut session);
 }
