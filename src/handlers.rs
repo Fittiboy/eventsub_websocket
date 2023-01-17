88158dba2e61bc5bd502ef6bbadd1b40c235b623
Fitti <dev@fitti.io>
2023-01-17 11:27:39 +0100

    Add custom errors
    Prepare custom errors to propagate back to user's main thread.

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,32 +1,59 @@
 use crate::types::{Session, TwitchMessage, Welcome};
 
+pub enum HandlerErr {
+    Welcome(WelcomeHandlerErr),
+}
+
 pub trait Handler
 where
     Self: std::fmt::Debug,
 {
-    fn handle(&self, session: Option<&mut Session>);
+    fn handle(&self, session: Option<&mut Session>) -> Result<(), HandlerErr>;
 }
 
 impl Handler for TwitchMessage {
-    fn handle(&self, session: Option<&mut Session>) {
+    fn handle(&self, session: Option<&mut Session>) -> Result<(), HandlerErr> {
         match self {
-            TwitchMessage::Welcome(msg) => msg.handle(session),
-            _ => {}
+            TwitchMessage::Welcome(msg) => {
+                msg.handle(session).map_err(|err| HandlerErr::Welcome(err))
+            }
+            TwitchMessage::Reconnect(msg) => todo!("{:#?}", msg),
+            _ => Ok(()),
         }
     }
 }
 
+pub enum WelcomeHandlerErr {
+    NoKeepalive(String),
+    InvalidKeepalive(String),
+    NoSession(String),
+}
+
 impl Welcome {
-    fn handle(&self, session: Option<&mut Session>) {
+    fn handle(&self, session: Option<&mut Session>) -> Result<(), WelcomeHandlerErr> {
         if let Some(session) = session {
-            session.set_id(self.payload().session().id().to_string());
-            if let Some(time) = &self.payload().session().keepalive() {
-                session.set_keepalive(time.as_u64().unwrap());
+            session.set_id(self.session_id().to_string());
+            if let Some(time) = &self.keepalive() {
+                let keepalive = time.as_u64();
+                match keepalive {
+                    Some(time) => session.set_keepalive(time),
+                    None => {
+                        return Err(WelcomeHandlerErr::InvalidKeepalive(format!(
+                            "invalid keepalive time received: {}",
+                            time
+                        )))
+                    }
+                }
             } else {
-                panic!("Twitch did not return a keepalive time!");
+                return Err(WelcomeHandlerErr::NoKeepalive(
+                    "Twitch did not return a keepalive time!".to_string(),
+                ));
             };
         } else {
-            panic!("Welcome message needs session!");
+            return Err(WelcomeHandlerErr::NoSession(
+                "Welcome handler needs to be called with valid session".to_string(),
+            ));
         };
+        Ok(())
     }
 }
